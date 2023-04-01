89ef00d254c48dc600c454ac6ba1f5d962acc0b6
Mohit Kashyap <92683467+MohitKashyapUK@users.noreply.github.com>
2023-04-01 22:29:36 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 from flask import Flask
 from flask_socketio import SocketIO, emit
-from flask_cors import CORS
+#from flask_cors import CORS
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'secret!'
 #app.config['CORS_ALLOWED_ORIGINS'] = '*'
-cors = CORS(app, resources={r"/*": {"origins": "*"}})
-socketio = SocketIO(app, cors_allowed_origins='*')
+#cors = CORS(app, resources={r"/*": {"origins": "*"}})
+socketio = SocketIO(app, cors_allowed_origins='*',  async_mode="eventlet")
 #, logger=True, engineio_logger=True)
 
 @app.route("/")
@@ -22,5 +22,6 @@ def message(message):
     emit(message)
 
 if __name__ == "__main__":
-    socketio.run(app, cors_allowed_origins='*')
+    socketio.run(app, host='0.0.0.0', port=5000, debug=True, use_reloader=True)
+    #, cors_allowed_origins='*')
     #, host='localhost', port=26543)
