3b6404775aea340eefac2caa712601b4f6e14aa6
Mohit Kashyap <92683467+MohitKashyapUK@users.noreply.github.com>
2023-04-01 16:49:36 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,21 @@
-from flask import Flask, jsonify
-import os
+from flask import Flask
+from flask_socketio import SocketIO
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'secret!'
+socketio = SocketIO(app, logger=True, engineio_logger=True)
 
-
-@app.route('/')
+@app.route("/")
 def index():
-    return jsonify({"Choo Choo": "Welcome to your Flask app 🚅"})
+    return "Hello!"
 
+@socketio.on('connect')
+def test_connect(auth):
+    emit('my response', {'data': f'Connected {auth}'})
 
-if __name__ == '__main__':
-    app.run(debug=True, port=os.getenv("PORT", default=5000))
+@socketio.on("message")
+def message(message):
+    emit(message)
+
+if __name__ == "__main__":
+    socketio.run(app)
