77fea4a73ef57c96e5c7780394fca2269ae71c78
Mohit Kashyap <92683467+MohitKashyapUK@users.noreply.github.com>
2023-04-02 20:45:12 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,21 +1,12 @@
+import eventlet
 from flask import Flask
 from flask_socketio import SocketIO, emit
 from flask_cors import CORS
-import eventlet
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'secret!'
 CORS(app)
-socketio = SocketIO(
-    app, 
-    logger=True, 
-    engineio_logger=True, 
-    reconnect=True, 
-    reconnect_timeout=5, 
-    ping_timeout=120, 
-    cors_allowed_origins='*', 
-    async_mode="eventlet"
-)
+socketio = SocketIO(app, cors_allowed_origins='*', async_mode="eventlet")
 
 @app.route("/")
 def index():
