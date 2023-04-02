7bdd531e3b9528c914dad43fff2e753bb97cc8a4
Mohit Kashyap <92683467+MohitKashyapUK@users.noreply.github.com>
2023-04-02 14:40:18 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,9 @@
 from flask import Flask
 from flask_socketio import SocketIO, emit
-#from flask_cors import CORS
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'secret!'
-#app.config['CORS_ALLOWED_ORIGINS'] = '*'
-#cors = CORS(app, resources={r"/*": {"origins": "*"}})
 socketio = SocketIO(app, cors_allowed_origins='*', async_mode="eventlet")
-#, logger=True, engineio_logger=True)
 
 @app.route("/")
 def index():
@@ -15,11 +11,11 @@ def index():
 
 @socketio.on('connect')
 def test_connect(auth):
-    emit('my response', {'data': 'Connected'})
+    emit('message', {'data': 'Connected'})
 
 @socketio.on("message")
 def message(message):
     emit(message)
 
 if __name__ == "__main__":
-    socketio.run(app, debug=True, host='0.0.0.0')
+    socketio.run(app)
