24a625f8aad3ffb390f78e39d7ae6ea64a309c10
Mohit Kashyap <92683467+MohitKashyapUK@users.noreply.github.com>
2023-04-03 18:40:24 +0530

    Update main.py

--- main.py
+++ main.py
@@ -19,51 +19,54 @@ def test_connect():
 def start(message):
     emit("message", { "data": "Starting!" }, broadcast=True)
 
-    import subprocess
-    output = subprocess.run(['su', '-'], capture_output=True).stdout.decode("utf-8")
-    emit("message", {"data": output}, broadcast=True)
-
-    output = subprocess.run(["apt-get", "update", "-y"], capture_output=True).stdout.decode("utf-8")
-    emit("message", { "data": output }, broadcast=True)
-
-    output = subprocess.run(["apt-get", "upgrade", "-y"], capture_output=True).stdout.decode("utf-8")
-    emit("message", { "data": output }, broadcast=True)
-
-    requirements = ['make', 'git', 'zlib1g-dev', 'libssl-dev', 'gperf', 'cmake', 'clang', 'libc++-dev', 'libc++abi-dev']
-    for i in requirements:
-        output = subprocess.run(['apt-get', 'install', i, '-y'], capture_output=True).stdout.decode("utf-8")
+    try:
+        import subprocess
+        output = subprocess.run(['su', '-'], capture_output=True).stdout.decode("utf-8")
+        emit("message", {"data": output}, broadcast=True)
+    
+        output = subprocess.run(["apt-get", "update", "-y"], capture_output=True).stdout.decode("utf-8")
+        emit("message", { "data": output }, broadcast=True)
+    
+        output = subprocess.run(["apt-get", "upgrade", "-y"], capture_output=True).stdout.decode("utf-8")
+        emit("message", { "data": output }, broadcast=True)
+    
+        requirements = ['make', 'git', 'zlib1g-dev', 'libssl-dev', 'gperf', 'cmake', 'clang', 'libc++-dev', 'libc++abi-dev']
+        for i in requirements:
+            output = subprocess.run(['apt-get', 'install', i, '-y'], capture_output=True).stdout.decode("utf-8")
+            emit('message', { 'data': output }, broadcast=True)
+    
+        output = subprocess.run(['exit'], capture_output=True).stdout.decode("utf-8")
         emit('message', { 'data': output }, broadcast=True)
-
-    output = subprocess.run(['exit'], capture_output=True).stdout.decode("utf-8")
-    emit('message', { 'data': output }, broadcast=True)
-
-    output = subprocess.run(['git', 'clone', '--recursive', 'https://github.com/tdlib/telegram-bot-api.git'], capture_output=True).stdout.decode("utf-8")
-    emit('message', { 'data': output }, broadcast=True)
-
-    output = subprocess.run(['cd', 'telegram-bot-api'], capture_output=True).stdout.decode("utf-8")
-    emit('message', { 'data': output }, broadcast=True)
-
-    output = subprocess.run(['rm', '-rf', 'build'], capture_output=True).stdout.decode("utf-8")
-    emit('message', { 'data': output }, broadcast=True)
-
-    output = subprocess.run(['mkdir', 'build'], capture_output=True).stdout.decode("utf-8")
-    emit('message', { 'data': output }, broadcast=True)
-
-    output = subprocess.run(['cd', 'build'], capture_output=True).stdout.decode("utf-8")
-    emit('message', { 'data': output }, broadcast=True)
-
-    compile = 'CXXFLAGS="-stdlib=libc++" CC=/usr/bin/clang CXX=/usr/bin/clang++ cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX:PATH=.. ..'.split()
-    output = subprocess.run(compile, capture_output=True).stdout.decode("utf-8")
-    emit('message', { 'data': output }, broadcast=True)
-
-    output = subprocess.run(['cmake', '--build', '.', '--target', 'install'], capture_output=True).stdout.decode("utf-8")
-    emit('message', { 'data': output }, broadcast=True)
-
-    output = subprocess.run(['cd', '../..'], capture_output=True).stdout.decode("utf-8")
-    emit('message', { 'data': output }, broadcast=True)
-
-    output = subprocess.run(['ls', '-l', 'telegram-bot-api/bin/telegram-bot-api*'], capture_output=True).stdout.decode("utf-8")
-    emit('message', { 'data': output }, broadcast=True)
+    
+        output = subprocess.run(['git', 'clone', '--recursive', 'https://github.com/tdlib/telegram-bot-api.git'], capture_output=True).stdout.decode("utf-8")
+        emit('message', { 'data': output }, broadcast=True)
+    
+        output = subprocess.run(['cd', 'telegram-bot-api'], capture_output=True).stdout.decode("utf-8")
+        emit('message', { 'data': output }, broadcast=True)
+    
+        output = subprocess.run(['rm', '-rf', 'build'], capture_output=True).stdout.decode("utf-8")
+        emit('message', { 'data': output }, broadcast=True)
+    
+        output = subprocess.run(['mkdir', 'build'], capture_output=True).stdout.decode("utf-8")
+        emit('message', { 'data': output }, broadcast=True)
+    
+        output = subprocess.run(['cd', 'build'], capture_output=True).stdout.decode("utf-8")
+        emit('message', { 'data': output }, broadcast=True)
+    
+        compile = 'CXXFLAGS="-stdlib=libc++" CC=/usr/bin/clang CXX=/usr/bin/clang++ cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX:PATH=.. ..'.split()
+        output = subprocess.run(compile, capture_output=True).stdout.decode("utf-8")
+        emit('message', { 'data': output }, broadcast=True)
+    
+        output = subprocess.run(['cmake', '--build', '.', '--target', 'install'], capture_output=True).stdout.decode("utf-8")
+        emit('message', { 'data': output }, broadcast=True)
+    
+        output = subprocess.run(['cd', '../..'], capture_output=True).stdout.decode("utf-8")
+        emit('message', { 'data': output }, broadcast=True)
+    
+        output = subprocess.run(['ls', '-l', 'telegram-bot-api/bin/telegram-bot-api*'], capture_output=True).stdout.decode("utf-8")
+        emit('message', { 'data': output }, broadcast=True)
+    except e:
+        emit("message", { "data": f"Error occured: {e}" })
 
 @socketio.on("message")
 def message(message):
